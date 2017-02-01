286b99cdde0b07ac138db09734aef6b987cef1a5
rnbronstein <rnbronstein@gmail.com>
2017-02-01 02:49:32 -0500

    Somehow the entire officials table migration was lost

--- db/migrate/20170201023252_create_officials.rb
+++ db/migrate/20170201023252_create_officials.rb
@@ -1,6 +1,15 @@
 class CreateOfficials < ActiveRecord::Migration[5.0]
   def change
     create_table :officials do |t|
+        t.string :first_name
+        t.string :last_name
+        t.string :full_name
+        t.datetime :birthday
+        t.string :gender
+        t.string :religion
+        t.string :bioguide_id
+
+        t.timestamps
     end
   end
 end
